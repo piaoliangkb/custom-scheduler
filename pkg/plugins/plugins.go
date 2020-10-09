b20a66c1c75b49a4adb8c9f647f862b5a17d08f3
piaoliangkb <piaoliangkb@gmail.com>
2020-10-09 13:08:16 +0800

    Fix Filter functions declaration

--- pkg/plugins/plugins.go
+++ pkg/plugins/plugins.go
@@ -7,29 +7,33 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/klog"
 	framework "k8s.io/kubernetes/pkg/scheduler/framework/v1alpha1"
+	"k8s.io/kubernetes/pkg/scheduler/nodeinfo"
 )
 
 type SamplePlugin struct{}
 
-var _ framework.PreFilterPlugin = SamplePlugin{}
-var _ framework.FilterPlugin = SamplePlugin{}
+var (
+	_ framework.PreFilterPlugin = &SamplePlugin{}
+	_ framework.FilterPlugin    = &SamplePlugin{}
+)
 
 // plugin-name
-const Name = "myplugin"
+const Name = "myscheduler"
 
-func (s SamplePlugin) Name() string {
+func (s *SamplePlugin) Name() string {
 	return Name
 }
 
-func (s SamplePlugin) PreFilter(ctx context.Context, state *framework.CycleState, pod *v1.Pod, nodeName string) *framework.Status {
+func (s *SamplePlugin) PreFilter(ctx context.Context, state *framework.CycleState, pod *v1.Pod) *framework.Status {
 	klog.V(3).Infof("This is custom scheduler stage of: PreFilter")
 	klog.V(3).Infof("prefilter pod: %v", pod.Name)
 	return framework.NewStatus(framework.Success, "")
 }
 
-func (s SamplePlugin) Filter(ctx context.Context, state *framework.CycleState, pod *v1.Pod, nodeName string) *framework.Status {
+//
+func (s *SamplePlugin) Filter(ctx context.Context, state *framework.CycleState, pod *v1.Pod, node *nodeinfo.NodeInfo) *framework.Status {
 	klog.V(3).Infof("This is custom scheduler stage of: Filter")
-	klog.V(3).Infof("filter pod: %v, node: %v", pod.Name, nodeName)
+	klog.V(3).Infof("filter pod: %v, node: %v", pod.Name, node.Node().Name)
 	return framework.NewStatus(framework.Success, "")
 }
 
