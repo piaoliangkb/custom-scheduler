0e103614abdb53c3024bf9d32198fabfedd675a7
piaoliangkb <piaoliangkb@gmail.com>
2020-10-27 20:05:38 +0800

    Split plugins into httpreq and simplelog

--- main.go
+++ main.go
@@ -6,7 +6,8 @@ import (
 	"os"
 	"time"
 
-	"myscheduler/pkg/plugins"
+	"myscheduler/pkg/httpreq"
+	"myscheduler/pkg/simplelog"
 
 	"k8s.io/component-base/logs"
 	"k8s.io/kubernetes/cmd/kube-scheduler/app"
@@ -15,13 +16,10 @@ import (
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
 
-	fmt.Println("scheduler name:", plugins.Name)
-
-	// 注册自定义 scheduler 插件
+	// 注册自定义 scheduler plugin
 	command := app.NewSchedulerCommand(
-		// plugins.Name 和 plugins.New 可以在
-		// myscheduler/pkg/plugin/ 文件夹中的 plugins package 中获得
-		app.WithPlugin(plugins.Name, plugins.New),
+		app.WithPlugin(httpreq.Name, httpreq.New),
+		app.WithPlugin(simplelog.Name, simplelog.New),
 	)
 
 	if err := command.Execute(); err != nil {
