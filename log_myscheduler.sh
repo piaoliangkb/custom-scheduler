9ba868112225105c2891876f5bbdcf5ee5faed95
piaoliangkb <piaoliangkb@gmail.com>
2020-10-28 12:13:18 +0800

    Change schedulerName & deployment name

--- log_myscheduler.sh
+++ log_myscheduler.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-podname=$(kubectl get po -n kube-system | grep myscheduler | awk '{print $1}')
+podname=$(kubectl get po -n kube-system | grep custom-scheduler | awk '{print $1}')
 kubectl logs -f $podname -n kube-system
